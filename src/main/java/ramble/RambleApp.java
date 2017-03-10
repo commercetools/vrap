4cd8567849c4f4f78c66e552142ba1804c2c52e6
mkoester <matthias.koester@commercetools.de>
2017-03-10 14:28:37 +0100

    Added support for raml api-console.

--- src/main/java/ramble/RambleApp.java
+++ src/main/java/ramble/RambleApp.java
@@ -1,5 +1,9 @@
 package ramble;
 
+import com.google.common.base.Charsets;
+import com.google.common.base.Joiner;
+import com.google.common.io.ByteSource;
+import com.google.common.io.Resources;
 import org.raml.v2.api.RamlModelBuilder;
 import org.raml.v2.api.RamlModelResult;
 import org.raml.v2.api.model.common.ValidationResult;
@@ -9,20 +13,24 @@ import org.raml.v2.api.model.v10.datamodel.ExampleSpec;
 import org.raml.v2.api.model.v10.datamodel.TypeDeclaration;
 import org.raml.v2.api.model.v10.methods.Method;
 import org.raml.v2.api.model.v10.resources.Resource;
-import org.raml.v2.internal.impl.commons.nodes.BodyNode;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import ratpack.file.MimeTypes;
+import ratpack.guice.Guice;
+import ratpack.handlebars.HandlebarsModule;
 import ratpack.handling.Context;
 import ratpack.handling.Handler;
-import ratpack.http.HttpMethod;
 import ratpack.http.Request;
 import ratpack.server.RatpackServer;
 
-import java.util.*;
+import java.net.URL;
+import java.util.Map;
+import java.util.Optional;
+import java.util.Set;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
-import static ratpack.jackson.Jackson.json;
+import static ratpack.handlebars.Template.handlebarsTemplate;
 
 
 class ResourcesHandler implements Handler {
@@ -60,6 +68,37 @@ class ResourcesHandler implements Handler {
     }
 }
 
+class ApiConsoleHandler implements Handler {
+    private final static String apiConsoleBase = "META-INF/resources/webjars/api-console/3.0.4/dist";
+
+    @Override
+    public void handle(final Context ctx) throws Exception {
+        final String path = ctx.getRequest().getPath();
+
+        if (path.equals("api-console/")) {
+            ctx.render(handlebarsTemplate("index.html"));
+        }
+        else if (path.startsWith("api-console/")) {
+            final String relativePath = path.substring("api-console/".length());
+            final String apiConsoleResourcePath = relativePath.isEmpty() ?
+                    Joiner.on("/").join(apiConsoleBase, "index.html") :
+                    Joiner.on("/").join(apiConsoleBase, relativePath);
+
+            final URL resource = Resources.getResource(apiConsoleResourcePath);
+            if (resource == null) {
+                ctx.next();
+            } else {
+                final ByteSource byteSource = Resources.asByteSource(resource);
+                final String content = byteSource.asCharSource(Charsets.UTF_8).read();
+
+                ctx.getResponse().send(ctx.get(MimeTypes.class).getContentType(apiConsoleResourcePath), content);
+            }
+        } else {
+            ctx.next();
+        }
+    }
+}
+
 public class RambleApp {
     private static Logger LOG = LoggerFactory.getLogger(RambleApp.class);
 
@@ -73,7 +112,9 @@ public class RambleApp {
             final Api api = ramlModelResult.getApiV10();
 
             RatpackServer.start(server -> server
-                    .handlers(chain -> chain.all(new ResourcesHandler(api)))
+                    .serverConfig(c -> c.findBaseDir())
+                    .registry(Guice.registry(b -> b.module(HandlebarsModule.class)))
+                    .handlers(chain -> chain.all(new ApiConsoleHandler()).all(new ResourcesHandler(api)))
             );
         }
 
