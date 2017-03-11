bb482c076376bc91ca8c72d7d3bd0e653ec84900
mkoester <matthias.koester@commercetools.de>
2017-03-11 19:31:33 +0100

    Improved loading of api-console files.

--- src/main/java/ramble/ApiConsoleHandler.java
+++ src/main/java/ramble/ApiConsoleHandler.java
@@ -1,33 +1,49 @@
 package ramble;
 
-import com.google.common.base.Charsets;
 import com.google.common.base.Joiner;
 import com.google.common.collect.ImmutableMap;
-import com.google.common.io.ByteSource;
 import com.google.common.io.Resources;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import ratpack.file.MimeTypes;
 import ratpack.handling.Context;
 import ratpack.handling.Handler;
 import ratpack.path.PathBinding;
 
+import java.io.IOException;
+import java.net.URI;
 import java.net.URL;
+import java.nio.file.FileSystems;
 import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Collections;
 
 import static ratpack.handlebars.Template.handlebarsTemplate;
+import static ratpack.util.Exceptions.uncheck;
 
 /**
  * Handler for the api-console integration.
  */
 class ApiConsoleHandler implements Handler {
     private final static Logger LOG = LoggerFactory.getLogger(ApiConsoleHandler.class);
-    private final static String apiConsoleBase = "META-INF/resources/webjars/api-console/3.0.4/dist";
+
+    private static final String API_CONSOLE_ROOT = "META-INF/resources/webjars/api-console/3.0.4/";
+    private final static String API_CONSOLE_ASSETS = API_CONSOLE_ROOT + "dist";
 
     private final Path fileName;
 
     public ApiConsoleHandler(final Path fileName) {
         this.fileName = fileName;
+        initJarFileSystem();
+    }
+
+    private void initJarFileSystem() {
+        final URL resource = Resources.getResource(API_CONSOLE_ROOT + "package.json");
+        final String[] split = resource.toString().split("!");
+        try {
+            FileSystems.newFileSystem(URI.create(split[0]), Collections.emptyMap());
+        } catch (IOException e) {
+            uncheck(e);
+        }
     }
 
     @Override
@@ -40,15 +56,11 @@ class ApiConsoleHandler implements Handler {
             ctx.render(handlebarsTemplate(ImmutableMap.of("fileName", fileName), "api-console/index.html"));
         }
         else  {
-            final String apiConsoleResourcePath = Joiner.on("/").join(apiConsoleBase, path);
+            final String apiConsoleResourcePath = Joiner.on("/").join(API_CONSOLE_ASSETS, path);
 
             try {
                 final URL resource = Resources.getResource(apiConsoleResourcePath);
-                final ByteSource byteSource = Resources.asByteSource(resource);
-                final String content = byteSource.asCharSource(Charsets.UTF_8).read();
-
-                final String contentType = ctx.get(MimeTypes.class).getContentType(apiConsoleResourcePath);
-                ctx.getResponse().send(contentType, content);
+                ctx.render(Paths.get(resource.toURI()));
             } catch (IllegalArgumentException e) {
                 LOG.error("Resource {} not found at {}", path, apiConsoleResourcePath);
             }
