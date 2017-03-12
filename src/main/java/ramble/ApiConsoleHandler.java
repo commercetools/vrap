7236bef7b79ec918c4d24829cd77c7a2777d4b41
mkoester <matthias.koester@commercetools.de>
2017-03-12 13:40:01 +0100

    Extracted webjar handling into separate class.

--- src/main/java/ramble/ApiConsoleHandler.java
+++ src/main/java/ramble/ApiConsoleHandler.java
@@ -1,49 +1,26 @@
 package ramble;
 
 import com.google.common.collect.ImmutableMap;
-import com.google.common.io.Resources;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import ratpack.handling.Context;
-import ratpack.handling.Handler;
 import ratpack.path.PathBinding;
 
-import java.io.IOException;
-import java.net.URI;
-import java.net.URL;
-import java.nio.file.FileSystem;
-import java.nio.file.FileSystems;
 import java.nio.file.Path;
-import java.util.Collections;
 
 import static ratpack.handlebars.Template.handlebarsTemplate;
-import static ratpack.util.Exceptions.uncheck;
 
 /**
  * Handler for the api-console integration.
  */
-class ApiConsoleHandler implements Handler {
+class ApiConsoleHandler extends WebJarHandler {
     private final static Logger LOG = LoggerFactory.getLogger(ApiConsoleHandler.class);
 
-    private static final String API_CONSOLE_ROOT = "META-INF/resources/webjars/api-console/3.0.4/";
-    private final static String API_CONSOLE_ASSETS = API_CONSOLE_ROOT + "dist";
-
     private final Path fileName;
-    private final FileSystem fileSystem;
 
     public ApiConsoleHandler(final Path ramlFile) {
+        super("api-console", "3.0.4", "dist");
         this.fileName = ramlFile;
-        this.fileSystem = initJarFileSystem();
-    }
-
-    private FileSystem initJarFileSystem() {
-        final URL resource = Resources.getResource(API_CONSOLE_ROOT + "package.json");
-        final String[] split = resource.toString().split("!");
-        try {
-            return FileSystems.newFileSystem(URI.create(split[0]), Collections.emptyMap());
-        } catch (IOException e) {
-            throw uncheck(e);
-        }
     }
 
     @Override
@@ -55,12 +32,7 @@ class ApiConsoleHandler implements Handler {
             ctx.render(handlebarsTemplate(ImmutableMap.of("fileName", fileName), "api-console/index.html"));
         }
         else  {
-            try {
-                final Path resourcePath = fileSystem.getPath(API_CONSOLE_ASSETS, path);
-                ctx.render(resourcePath);
-            } catch (IllegalArgumentException e) {
-                LOG.error("Resource {} not found at {}", path, API_CONSOLE_ASSETS);
-            }
+            super.handle(ctx);
         }
     }
 }
---
+++ src/main/java/ramble/WebJarHandler.java
@@ -0,0 +1,64 @@
+package ramble;
+
+import com.google.common.base.Joiner;
+import com.google.common.io.Resources;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import ratpack.handling.Context;
+import ratpack.handling.Handler;
+import ratpack.path.PathBinding;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.URL;
+import java.nio.file.FileSystem;
+import java.nio.file.FileSystems;
+import java.nio.file.Path;
+import java.util.Collections;
+
+import static ratpack.util.Exceptions.uncheck;
+
+/**
+ * Handler for loading assets from a web jar.
+ */
+class WebJarHandler implements Handler {
+    private final static Logger LOG = LoggerFactory.getLogger(WebJarHandler.class);
+
+    private static final String WEBJAR_ROOT = "META-INF/resources/webjars";
+
+    private final FileSystem jarFileSystem;
+    private final String modueName;
+    private final String version;
+    private final String bindPath;
+
+    public WebJarHandler(final String moduleName, final String version, final String bindPath) {
+        this.jarFileSystem = initJarFileSystem(moduleName, version);
+        this.modueName = moduleName;
+        this.version = version;
+        this.bindPath = bindPath;
+    }
+
+    private FileSystem initJarFileSystem(final String moduleName, final String version) {
+        final String webJarPath = Joiner.on("/").join(WEBJAR_ROOT, moduleName, version);
+        final URL resource = Resources.getResource(webJarPath);
+        final String[] split = resource.toString().split("!");
+        try {
+            return FileSystems.newFileSystem(URI.create(split[0]), Collections.emptyMap());
+        } catch (IOException e) {
+            throw uncheck(e);
+        }
+    }
+
+    @Override
+    public void handle(final Context ctx) throws Exception {
+        final PathBinding pathBinding = ctx.getPathBinding();
+        final String path = pathBinding.getPastBinding();
+
+        try {
+            final Path resourcePath = jarFileSystem.getPath(WEBJAR_ROOT, modueName, version, bindPath, path);
+            ctx.render(resourcePath);
+        } catch (IllegalArgumentException e) {
+            LOG.error("Resource {} not found", path);
+        }
+    }
+}
